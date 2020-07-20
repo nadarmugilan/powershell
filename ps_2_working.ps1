25113393deb4f6e91b89a8129d45abf043e284cb
nadarmugilan <nadarmugilan@gmail.com>
2020-07-20 09:31:08 +0530

    Create ps_2_working.ps1

---
+++ ps_2_working.ps1
@@ -0,0 +1,5 @@
+﻿get-content -path C:\Users\Mugilan\Desktop\computers.txt | 
+  Where {$_ -AND (Test-Connection $_ -Quiet)} |
+    foreach { Get-Hotfix -computername $_ -Credential (Get-Credential -Credential Administrator ) } | 
+      Select CSName,Description,HotFixID,InstalledBy,InstalledOn |
+        convertto-csv | out-file "C:\test.csv"
